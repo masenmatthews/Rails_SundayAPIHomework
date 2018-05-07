9d3873c68c2f9da8fc43db62e656f34b1291fbc4
Masen Matthews <masenmatthews@gmail.com>
2018-05-06 21:01:51 -0700

    complete homework

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,7 +1,9 @@
 class QuotesController < ApplicationController
 
   def index
-    @quotes = Quote.all
+    name = params[:name]
+    binding.pry
+    @quotes = Quote.search(name)
     json_response(@quotes)
   end
 
@@ -11,14 +13,18 @@ class QuotesController < ApplicationController
   end
 
   def create
-    @quote = Quote.create(quote_params)
-    json_response(@quote)
+    @quote = Quote.create!(quote_params)
+    json_response(@quote, :created)
   end
 
   def update
-    @quote = Quote.find(params[:id])
-    @quote.update(quote_params)
+    if @quote.update!(quote_params)
+      render status: 200, json: {
+       message: "Your quote has been updated successfully."
+       }
+    end
   end
+
 
   def destroy
     @quote = Quote.find(params[:id])
@@ -26,9 +32,6 @@ class QuotesController < ApplicationController
   end
 
   private
-  def json_response(object, status = :ok)
-    render json: object, status: status
-  end
 
   def quote_params
     params.permit(:author, :content)
